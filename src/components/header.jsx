2205fe003c71c618d48fd9d6a56985baf1a7aadb
Martinoux <martinblanckaert@yhaoo.fr>
2022-05-04 09:42:45 +0200

    organized files

---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import '../styles/header.css';
+import logo from "../assets/Logo.png";
+
+export default function Header() {
+    return (
+        <div className="topContainer">
+            <a href="/src/pages" target="_blank" rel="noopener noreferrer">
+                <div className="logoContainer">
+                    <img className="logoImg" src={logo} alt="logo" />
+                    <div className="logoText">
+                        Solar <br/> Predictor
+                    </div>
+                </div>
+            </a>
+            <div className="endContainer">
+                <div className="aboutContainer">
+                    About
+                </div>
+                <div className="moreContainer">
+                    More models
+                </div>
+            </div>
+        </div>
+    )
+}
