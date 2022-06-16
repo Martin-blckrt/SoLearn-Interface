cf583e5ecbb7f7d2f11e3af4ebd2e0a292d2b1c5
Eclairnoir12 <eclairnoir12@gmail.com>
2022-06-16 11:13:11 +0200

    Update Results.js

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -12,6 +12,7 @@ import {
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import Header from "../../components/header";
+import fileDownload from 'js-file-download';
 
 export default function Results(props) {
 	ChartJS.register(
@@ -24,20 +25,29 @@ export default function Results(props) {
 		Legend
 	);
 
-	const predictions_to_display = {};
-	let sum = 0;
+    const predictions_to_display = {};
+    let sum = 0;
+    let length = 0;
 
-	for (const dt in props.datas.prediction) {
-		predictions_to_display[new Date(Number(dt)).toLocaleString()] =
-			props.datas.prediction[dt];
-		sum += props.datas.prediction[dt];
-	}
+    for(const dt in props.datas.prediction){
+        predictions_to_display[new Date(Number(dt)).toLocaleString()] = props.datas.prediction[dt];
+        sum += props.datas.prediction[dt];
+        length++;
+    }
 
-	const total = (sum / props.datas.prediction.length) * 48;
+    const total = sum/length*48;
 
-	console.log(predictions_to_display);
+    console.log(props.datas.prediction, props.datas.prediction.length);
 
-	const options = {
+    const data = {
+        datasets: [{
+            backgroundColor: '#005403',
+            borderColor: '#5b8d44',
+            data: predictions_to_display,
+        }]
+    };
+
+    const options = {
 		responsive: true,
 		maintainAspectRation: false,
 		plugins: {
@@ -51,42 +61,32 @@ export default function Results(props) {
 			},
 		},
 	};
-
-	const data = {
-		datasets: [
-			{
-				backgroundColor: "#005403",
-				borderColor: "#5b8d44",
-				data: predictions_to_display,
-			},
-		],
-	};
-
-	return (
-		<div>
-			<Header />
-			<div className="pageResultsContainer">
-				<div className="leftResultsContainer">
-					<div className="resultsTitle">
-						Estimated Production for{" "}
-						<span className="locationTitle">{props.city_name}</span> based on
-						the next <span className="locationTitle">48h</span>
-					</div>
-					<div className="graphContainer">
-						<Line options={options} height={"100%"} data={data} />
-					</div>
-				</div>
-				<div className="downloadContainer">
-					<FileDownloadOutlined sx={{ marginRight: "4px" }} />
-					Download the data as a json file
-				</div>
-				<div className="rightResultsContainer">
-					<div className="resultsTitle">Financial Estimate</div>
-					<div className="financialContainer">
-						<FinancialEstimate total_estimated={total.toFixed(3)} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    
+    return(
+        <div>
+            <Header/>
+            <div className="pageResultsContainer">
+                <div className="leftResultsContainer">
+                    <div className="resultsTitle">
+                        Estimated Production for <span className="locationTitle">{props.city_name}</span> based on the next <span className="locationTitle">48h</span>
+                    </div>
+                    <div className="graphContainer">
+                        <Line options={options} height={"100%"} data={data} />
+                    </div>
+                </div>
+                <div className="downloadContainer" onClick={()=>{fileDownload(JSON.stringify(props.datas.prediction), `estimated_production_${(new Date()).toLocaleDateString().replace("/","_")}.json`)}} >
+                    <FileDownloadOutlined sx={{ marginRight: "4px"}}/>
+                    Download the data as a json file
+                </div>
+                <div className="rightResultsContainer">
+                    <div className="resultsTitle">
+                        Financial Estimate
+                    </div>
+                    <div className="financialContainer">
+                        <FinancialEstimate total_estimated={total.toFixed(3)}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
